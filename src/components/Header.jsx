0ca5378853e92a53494089355753fe2a5e668f4e
Anant-1 <54532485+Anant-1@users.noreply.github.com>
2024-01-14 05:07:31 +0530

    Changer Loader case in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ import { FiBell } from "react-icons/fi";
 import { CgClose } from "react-icons/cg";
 
 import { Context } from "../context/contextApi";
-import Loader from "../shared/loader";
+import Loader from "../shared/Loader";
 import { AvatarGenerator } from 'random-avatar-generator';
 
 const Header = () => {
