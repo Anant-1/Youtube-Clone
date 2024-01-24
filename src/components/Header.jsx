7ab77d95e6a02ff40f5a1e448bc05bab74807623
Anant-1 <54532485+Anant-1@users.noreply.github.com>
2024-01-24 22:50:38 +0530

    Dark mode bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
     const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
     return (
-        <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+        <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-black">
             {loading && <Loader />}
 
             <div className="flex h-5 items-center">
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,20 @@
-const base_url = 'https://api.apilayer.com/youtube';
-// const base_url = 'https://youtube138.p.rapidapi.com';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': import.meta.env.VITE_APP_YOUTUBE_API_KEY_1,
-// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-// 	}
-// };
-var myHeaders = new Headers();
-myHeaders.append("apikey", import.meta.env.VITE_APP_YOUTUBE_API_KEY_2);
-
-var options = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
+// const base_url = 'https://api.apilayer.com/youtube';
+const base_url = 'https://youtube138.p.rapidapi.com';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': import.meta.env.VITE_APP_YOUTUBE_API_KEY_1,
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
 };
+// var myHeaders = new Headers();
+// myHeaders.append("apikey", import.meta.env.VITE_APP_YOUTUBE_API_KEY_2);
+
+// var options = {
+//   method: 'GET',
+//   redirect: 'follow',
+//   headers: myHeaders
+// };
 export const fetchDataFromApi = async(url) =>{
   try{
     const searchQuery = `${base_url}/${url}`
